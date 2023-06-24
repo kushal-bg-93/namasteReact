48f28aebf90099162790808076af3cb0bbdf8fec
“kushal.govindaraju” <kushal.govindaraju@waycool.in>
2023-06-25 03:12:10 +0530

    test react app using parcel completed

---
+++ reactApp/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+{/* <div id="parent">
+    <div id="child1">
+        <h1>This is heading from child1</h1>
+        <h2>This is second heading from child 1</h2>
+    </div>
+    <div id="child2">
+        <h1>This is heading from child 2</h1>
+        <h2>This is second heading from child 2</h2>
+    </div>
+</div> */}
+
+
+
+// const heading=React.createElement('h1',{id:"heading"},"This is heading from react");
+
+const parent=React.createElement('div',{id:"parent"},[React.createElement('div',{id:"child1"},[React.createElement('h1',{},"This is heading from child1 from react"),React.createElement("h2",{},"This is second heading from child 1")]),React.createElement("div",{id:"child2"},[React.createElement('h1',{},"This is heading from child 2"),React.createElement('h2',{},"This is second heading from child 2")])])
+
+    const root=ReactDOM.createRoot(document.getElementById("root"))
+
+    root.render(parent);
