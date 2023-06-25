c34961e6678deb416abd0b8f7d7bb3be37359897
“kushal.govindaraju” <kushal.govindaraju@waycool.in>
2023-06-25 17:17:33 +0530

    react component vs react element

--- reactApp/App.js
+++ reactApp/App.js
@@ -1,23 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-{/* <div id="parent">
-    <div id="child1">
-        <h1>This is heading from child1</h1>
-        <h2>This is second heading from child 1</h2>
-    </div>
-    <div id="child2">
-        <h1>This is heading from child 2</h1>
-        <h2>This is second heading from child 2</h2>
-    </div>
-</div> */}
+const heading=<h1>This is heading from react element</h1>
 
+const Heading=()=>(
+        <>
+        {heading}
+    <h1>This is heading from react component</h1>
+        </>
 
+)
 
-// const heading=React.createElement('h1',{id:"heading"},"This is heading from react");
+const root=ReactDOM.createRoot(document.getElementById('root'))
 
-const parent=React.createElement('div',{id:"parent"},[React.createElement('div',{id:"child1"},[React.createElement('h1',{},"This is heading from child1 from react"),React.createElement("h2",{},"This is second heading from child 1")]),React.createElement("div",{id:"child2"},[React.createElement('h1',{},"This is heading from child 2"),React.createElement('h2',{},"This is second heading from child 2")])])
-
-    const root=ReactDOM.createRoot(document.getElementById("root"))
-
-    root.render(parent);
+root.render(<Heading/>)
