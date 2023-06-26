03057015c48cbe8709ce673ac2cfc4ab7733aec5
“kushal.govindaraju” <kushal.govindaraju@waycool.in>
2023-06-27 01:02:57 +0530

    useState completed

--- swiggy/src/components/BodyComponent.js
+++ swiggy/src/components/BodyComponent.js
@@ -1,13 +1,22 @@
 import RestroCard from "./RestroCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
-const BodyComponent=()=>(
+const BodyComponent=()=>{
+
+    [restrauntList,setRestrauntList]=useState(resList)
+
+    const filterRestraunt=()=>{
+        restrauntList=setRestrauntList(restrauntList.filter((restraunt)=>restraunt.data.avgRating>=4))
+    }
+    
+    return (
     <>
-    <div className="searchComponent"> Search</div>
+    <div className="filter"> <button className="filter-btn" onClick={filterRestraunt}>Filter</button></div>
     <div className="restroContainer">
-        {resList.map((restraunt)=><RestroCard key={restraunt.data.id} resData={restraunt}/>)}
+        {restrauntList.map((restraunt)=><RestroCard key={restraunt.data.id} resData={restraunt}/>)}
     </div>
     </>
-)
+)}
 
 export default BodyComponent;
