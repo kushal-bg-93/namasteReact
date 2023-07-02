a7e409262deb9d52ee8617dbcb1d04e306054425
“kushal.govindaraju” <kushal.govindaraju@waycool.in>
2023-07-03 02:00:25 +0530

    visually enhanced v2

--- swiggy/src/components/BodyComponent.js
+++ swiggy/src/components/BodyComponent.js
@@ -32,7 +32,7 @@ const BodyComponent=()=>{
     
     return (!restrauntList.length)?(<Shimmer/>):(
     <div>
-    <div className="m-4 flex justify-between"><button className="py-2 px-5 rounded-lg text-gray-400 hover:text-gray-300 duration-150 bg-gray-800 " onClick={filterRestraunt}>Filter</button><div> <input type="text" className="border rounded-lg p-2 ml-4 mr-4 shadow-sm" value={searchText} onChange={(e)=>setSearchText(e.target.value)}/><button className="py-2 px-5 hover:text-gray-300 duration-150 rounded-lg text-gray-400 bg-gray-800 " onClick={searchRestaurant}>Search</button></div></div>
+    <div className="m-4 flex justify-between"><button className="py-2 px-5 rounded-lg text-gray-400 hover:text-gray-300 duration-150 bg-gray-800 " onClick={filterRestraunt}>Filter</button><div> <input type="text" className=" border border-gray-400 rounded-lg p-2 ml-4 mr-4 shadow-sm" value={searchText} onChange={(e)=>setSearchText(e.target.value)}/><button className="py-2 px-5 hover:text-gray-300 duration-150 rounded-lg text-gray-400 bg-gray-800 " onClick={searchRestaurant}>Search</button></div></div>
     <div className="flex flex-wrap justify-start ml-20 mx-auto">
         {restrauntList.map((restraunt)=><Link key={restraunt.data.id} to={"/menu/"+restraunt.data.id}><RestroCard resData={restraunt}/></Link>)}
     </div>
--- swiggy/src/components/RestroCard.js
+++ swiggy/src/components/RestroCard.js
@@ -3,9 +3,11 @@ const RestroCard=({resData})=>{
     const{name,costForTwo,cuisines,cloudinaryImageId,avgRating}=resData?.data;
     return (
       <div className="w-96 m-5 h-[440] border p-6 hover:border shadow-md hover:shadow-lg rounded-lg hover:scale-110 duration-150">
+          {avgRating<3.5?<h1 className="px-6 bg-red-600 z-10 text-white absolute">50% OFF</h1>:""}
           <img className="rounded-lg shadow-lg opacity-90 hover:opacity-100 hover:shadow-xl hover:scale-105 duration-200" src={CLOUDINARY_URL+cloudinaryImageId} alt="" />
           <h1 className="font-extrabold text-2xl mt-3">{name}</h1>
           <p className="text-gray-500">{cuisines?cuisines.join(', '):""}</p>
+          <hr className="mt-4"/>
           <div className="flex mt-6 justify-between items-end">
           <h2 className="font-bold text-gray-800">Rs.{costForTwo/100} For Two</h2>
           <div><h3 className={avgRating>3.5?'bg-green-600 rounded-sm shadow-2xl text-white px-4':'bg-orange-600 rounded-sm shadow-xl text-white px-4'}>{avgRating}</h3></div>
